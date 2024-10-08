6ce4dc195f8dfbc1278de7fd8f51c9d0af0d594b
Melashenko Ivan <melashenkoivan98@gmail.com>
2024-10-08 09:53:40 +0300

    разбил на 3 отдельные фуннкии, команда go test ./... -count=1 мне выдает ok      precode.go      0.167s всегда чтобы я не делалб только время менятся

--- precode_test.go
+++ precode_test.go
@@ -10,38 +10,45 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
-
+func TestMainStatus200(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// здесь нужно добавить необходимые проверки
-	// проверка на статус 200 ок
+	// Проверка на статус 200 OK
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	//Проверяем кол-во выдаваемых городов
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-
-	assert.Len(t, list, totalCount)
 
 }
 
-func ErrorStatusGerNotcity(t *testing.T) {
-	erorrReq := httptest.NewRequest("GET", "/cafe?count=10&city=notcity", nil) //запрос к сервису с несуществующим городом
-
+func TestMainHandlerWhenWrongCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=notcity", nil)
 	responseRecorder := httptest.NewRecorder()
-	erorrHandlerr := http.HandlerFunc(mainHandle)
-	erorrHandlerr.ServeHTTP(responseRecorder, erorrReq)
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
 
-	// проверяем статус 400
+	// Проверяем статус 400
 	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 
-	// проверяем запись при вводе несуществующего города
+	// Проверяем запись при вводе несуществующего города
 	expectedErrorMessage := "wrong city value"
 	assert.Equal(t, expectedErrorMessage, responseRecorder.Body.String())
+}
 
+func TestMainСities(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// Проверка на статус 200 OK
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+
+	// Конвертируем полученную строку в слайс
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	// Проверка на общее количество
+	assert.Len(t, list, totalCount)
 }
